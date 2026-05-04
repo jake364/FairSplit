cd706e537645b499d0ed0d3abfb358e74b98872b
Benjamin <benvah27@gmail.com>
2026-05-04 13:41:31 -0400

    Implement roommate management functions

---
+++ frontend/js/roommates.js
@@ -0,0 +1,38 @@
+function loadRoommates() {
+  $.get(API + "/roommates", function(roommates) {
+    $("#roommateList").empty()
+    
+    for (let i = 0; i < roommates.length; i++) {
+      $("#roommateList").append("<li>" + roommates[i].name + "</li>")
+    }
+  })
+}
+
+function addRoommate() {
+  const name = $("#roommateName").val()
+  
+  if (name === "") {
+    return
+  }
+
+  $.ajax({
+    url: API_URL + "/roommates",
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({
+      name: name
+    }),
+    success: function() {
+      $("#roommateName").val("")
+      loadRoommates()
+    }
+  })
+}
+
+$("#addRoommateBtn").on("click", function() {
+  addRoommate()
+})
+
+$(document).ready(function() {
+  loadRoommates()
+})
